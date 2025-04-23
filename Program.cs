f1dc8e6641bfb420363a30e053706a68fbb9ed47
Ste_home <smontuor@student.42firenze.it>
2025-04-23 09:17:39 +0200

    fix cors policy

--- Program.cs
+++ Program.cs
@@ -2,10 +2,28 @@ using Yarp.ReverseProxy;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Aggiunge YARP
+// 1. Configura la policy CORS
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowFrontend", policy =>
+    {
+        policy.WithOrigins("http://localhost:4200") // oppure * ma solo per test
+              .AllowAnyHeader()
+              .AllowAnyMethod();
+              //.AllowCredentials(); // in futuro per cookie o auth header
+    });
+});
+
+// 2. Aggiungi YARP
 builder.Services.AddReverseProxy()
     .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
 
 var app = builder.Build();
+
+// 3. Applica la policy CORS PRIMA del reverse proxy
+app.UseCors("AllowFrontend");
+
 app.MapReverseProxy();
+
 app.Run();
+
